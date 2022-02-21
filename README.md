78ad9a328b85dc6afb883c46a14c04f23afc7de7
Juozas <juozasgaigalas@gmail.com>
2022-02-22 00:14:12 +0200

    svelte wip

--- README.md
+++ README.md
@@ -1,41 +1,13 @@
 # Flux Web UI
 
-## Project Status
-
-:warning: This project is currently **on hold**, because the maintainers do not have capacity to
-develop it further at present. We (the Flux project) will try to keep it up to date with the Flux
-APIs so it will still run, but it will remain experimental and may not always work (please file an
-issue if you find it does not work!).
-
-Please get in touch in [#flux-contributors](https://slack.cncf.io/) if you are interested in helping
-to get this project moving again.
-
-See [the monitoring section of the Flux documentation](https://fluxcd.io/docs/guides/monitoring/)
-for how to install Flux's Grafana dashboards.
-
-## Installation
-
-To install the Flux Web UI:
-
-1. Download the latest release from the [releases page](https://github.com/fluxcd/webui/releases)
-2. Extract the binary from the downloaded archive
-3. Run the server in your terminal: `./flux-webui`
-4. You will see a log message letting you know startup was successful: `2021-06-03T13:26:37.552-0700 INFO Serving on port 9000`
-5. Navigate to <http://localhost:9000>
 
 ## Development
 
-To set up a development environment
+To run the frontend:
 
-1. Install `go` v1.16
-2. Install `Node.js v16.x.x`
-3. Install `reflex` for automated server builds: `go get github.com/cespare/reflex`
-4. [Install `kubebuilder`](https://book.kubebuilder.io/quick-start.html#installation)
-5. `npm install` to install UI dependencies
-6. `npm start` to start the frontend dev server (with hot-reloading)
+0. Install `Node.js v16.x.x` and `yarn`
+1. `cd ui`
+2. `yarn install`
+3. `yarn dev`
+4. Open http://127.0.0.1:1234
 
-To run tests:
-
-```shell
-make test
-```
--- ui/src/components/App.svelte
+++ ui/src/components/App.svelte
@@ -1,72 +1,62 @@
 <script lang="ts">
-	export let name: string = 'Flux Web UI';
-	import Select, { Option } from '@smui/select';
-  import Icon from '@smui/select/icon';
+  export let name: string = 'Flux Web UI uwu';
 
-	let contextNames = ['gcp', 'kind-kind'];
-	let selectedContext: string = contextNames[0];
+  let contextNames = ['gcp', 'kind-kind'];
+  let selectedContext: string = contextNames[0];
 
-	let selectedNamespace = 'all';
+  let selectedNamespace = 'all';
 </script>
 
 <main>
-	<div class="topnav">
-		<div>
-			<Select bind:value={selectedContext} label="Context" variant="outlined">
-				<Icon color="primary" class="material-icons" slot="leadingIcon">hub</Icon>
-				{#each contextNames as cname}
-					<Option value={cname}>{cname}</Option>
-				{/each}
-			</Select>
-	
-			<pre class="status">Context: {selectedContext}</pre>
-		</div>
+  <div class="topnav">
+    <div>
+      <p>select context</p>
+      <pre class="status">Context: {selectedContext}</pre>
+    </div>
 
-		<div>
-			<Select bind:value={selectedNamespace} label="Namespace">
-				<Icon color="secondary" class="material-icons" slot="leadingIcon">spoke</Icon>
-					<Option value=all>all</Option>
-			</Select> 
-		</div>
-	</div>
-	
-	<h1>Hello {name}!</h1>
+    <div>
+      <p>selected namespace</p>
+    </div>
+  </div>
+
+  <h1>Hello {name}!</h1>
 
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 0em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  :global(body) {
+    margin: 0;
+  }
 
-	.topnav {
-		display: flex;
-		justify-content: flex-end;
-		background: blue;
-		color: white !important;
-		padding: 1rem;
-	}
+  main {
+    text-align: center;
+    padding: 0em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	.material-icons {
-		color: white !important;
-	}
+  .topnav {
+    display: flex;
+    justify-content: flex-end;
+    background: blue;
+    color: white !important;
+    padding: 1rem;
+  }
 
-	.topnav > div {
-		margin: 0 1rem;
-	}
+  .topnav > div {
+    margin: 0 1rem;
+  }
 
-	h1 {
-		color: purple;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: purple;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
